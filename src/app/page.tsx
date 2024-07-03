94f7de0f428fc7c88bfc26c07c29562ef20c7037
Aowae <1170511756@qq.com>
2024-07-03 16:53:19 +0800

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/Button";
+
+export default function Home() {
+    return (
+        <div>
+            Root Page{" "}
+            <Button  type="submit">
+                Click Me
+            </Button>
+        </div>
+    );
+}
