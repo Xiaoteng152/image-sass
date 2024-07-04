5cf24981f061721bd897e0de6ccd4b3e6373521d
Aowae <1170511756@qq.com>
2024-07-04 22:37:17 +0800

    feat:主页

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,20 @@
 import { Button } from "@/components/Button";
-
+import { Input } from "@/components/Input";
+import { Textarea } from "@/components/Textarea";
 export default function Home() {
-    return (
-        <div>
-            Root Page{" "}
-            <Button  type="submit">
-                Click Me
-            </Button>
-        </div>
-    );
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <form className="w-full max-w-md flex flex-col gap-4">
+         <h1 className="text-center text-2xl  font-bold">Create Your App</h1>
+      <Input name="name" placeholder="App name"></Input>
+      <Textarea name="description" placeholder="description"></Textarea>
+      <Button
+        type="submit"
+        className="bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300"
+      >
+        Submit
+      </Button>
+      </form>
+    </div>
+  );
 }
