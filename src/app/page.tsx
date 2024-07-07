df0c002ae23639e62ee5dd88d1aac68a3e97efa4
Aowae <1170511756@qq.com>
2024-07-07 15:27:17 +0800

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
         type="submit"
         className="bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300"
       >
-        Submit
+        Submitwwww
       </Button>
       </form>
     </div>
